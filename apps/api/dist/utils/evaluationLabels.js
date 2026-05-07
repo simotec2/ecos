acd37a49b476372cd0e651c411a62ef5846b2766
simotec2 <figueroacasas.hernan@gmail.com>
2026-05-07 11:45:34 -0400

    fix: final report date parsing

--- apps/api/dist/utils/evaluationLabels.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.evaluationLabels = void 0;
-exports.evaluationLabels = {
-    PETS: "Evaluación Conductual",
-    ICOM: "Perfil Psicolaboral",
-    SECURITY: "Evaluación de Seguridad Operacional"
-};
